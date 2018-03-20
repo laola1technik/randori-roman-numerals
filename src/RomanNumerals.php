976d3cedd90909fd1be87e21c1a9c9aafc8971fc
Karl Zierhofer <karl.zierhofer@laola1.at>
2018-03-20 10:34:00 +0100

    Convert 1 to I

---
+++ src/RomanNumerals.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: karl.zierhofer
+ * Date: 20.03.2018
+ * Time: 10:21
+ */
+
+namespace App;
+
+
+class RomanNumerals
+{
+    /**
+     * RomanNumerals constructor.
+     */
+    public function __construct()
+    {
+    }
+
+    public function convert($int)
+    {
+        return "I";
+    }
+
+
+}
---
+++ test/RomanNumeralsTest.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace test;
+
+
+use App\RomanNumerals;
+
+class RomanNumeralsTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @test
+     */
+    public function it_should_return_I_for_1()
+    {
+        $romanNumerals = new RomanNumerals();
+
+        $this->assertEquals('I', $romanNumerals->convert(1));
+    }
+
+}
