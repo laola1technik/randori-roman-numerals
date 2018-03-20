ea2fbe1957ed3361807e763bf5f36e599ab6df08
Karl Zierhofer <karl.zierhofer@laola1.at>
2018-03-20 11:11:37 +0100

    throw exception on unknown literal.

--- src/RomanNumerals.php
+++ src/RomanNumerals.php
@@ -8,7 +8,6 @@ class RomanNumerals
 
     public function convert($arabicNumber)
     {
-
         switch ($arabicNumber) {
             case 1: return "I";
             case 5: return "V";
@@ -17,6 +16,7 @@ class RomanNumerals
             case 100: return "C";
             case 500: return "D";
             case 1000: return "M";
+            default: throw new \InvalidArgumentException($arabicNumber);
         }
     }
 
